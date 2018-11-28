187e05eee912a5ec6b41e805f7774635a1fe79f0
Martin Smith <martins@gmail.com>
2018-11-28 12:21:02 +0000

    Integrate plotting functions

---
+++ R/plots.R
@@ -0,0 +1,53 @@
+#' Initialize ternary plots for quartet plotting
+#' @importFrom Ternary TernaryPlot
+#' @export
+clInitializeTernaryQuarts <- function (zoom = 1, padding = 0.1, gridLines = 10,
+                                       fontSize = 1,
+                                       gridCol = rgb(0.92, 0.92, 0.92),
+                                       backgroundCol = rgb(0.985, 0.985, 0.992)) {
+  xLim <- c(0, 1 / zoom) - 0.01
+  yLim <- c(0.5-(1 / zoom), 0.5)
+  lab <- if (zoom == 1) c("Unresolved quartets",
+                          "Different quartets",
+                          "Identical quartets") else rep('', 3)
+
+  TernaryPlot(atip=NULL, btip=NULL, ctip=NULL,
+              alab=lab[1], blab=lab[2], clab=lab[3],
+              lab.cex=fontSize, lab.offset=0.13,
+              point='right', isometric = TRUE,
+              col=backgroundCol,
+              grid.lty='solid', grid.col=gridCol, grid.lines=gridLines,
+              grid.minor.lines = 0,
+              axis.labels =
+                if(zoom==1) round(seq(0, choose(22, 4), length.out=20), 0)
+              else if (zoom == 3.5) round(seq(0, choose(22, 4), length.out=20), 0)
+              else FALSE,
+              axis.col=rgb(0.6, 0.6, 0.6),
+              ticks.length = if (zoom == 1) 0.025 else 0.009,
+              #axis.labels.col = if (zoom == 1) gridCol else rgb(0.6, 0.6, 0.6),
+              padding=padding, xlim=xLim, ylim=yLim)
+  HorizontalGrid(gridLines)
+  AddToTernary(lines, list(c(0, 2/3, 1/3), c(1, 0, 0)), lty='dotted',
+               col=cbPalette8[8], lwd=2)
+}
+
+#' Initialize ternary plots for partition plotting
+#' @export
+clInitializeTernarySplits <- function(fontSize = 1,
+                                      gridCol = rgb(0.92, 0.92, 0.92)) {
+  TernaryPlot(NULL, NULL, NULL, #'Unresolved', 'Different', 'Same',
+              alab="Unresolved partitions",
+              blab="Different partitions",
+              clab="Identical partitions",
+              #alab=expression("Unresolved partitions" %->% ''),
+              #blab=expression("" %<-% "Different partitions"),
+              #clab=expression("Identical partitions" %->% ""),
+              lab.cex=fontSize, lab.offset=0.12,
+              col=BG_COL, point='right',
+              grid.lines = 19, grid.lty='solid', grid.col=gridCol,
+              grid.minor.lines = 0,
+              axis.col=rgb(0.6, 0.6, 0.6),
+              padding=0.1, axis.labels = 0:19)
+
+  HorizontalGrid(grid.col='#888888', grid.lines=19)
+}
--- R/points.R
+++ R/points.R
@@ -31,9 +31,48 @@ clPlotAverageQuartets <- function(dataset, cex=1.1, pch=2, col=clColours, ...) {
   invisible()
 }
 
-#' @describeIn clPlotQuartets
+#' @describeIn clPlotQuartets Plot average for single dataset across all 100 trees
 #' @export
 clPlotTheseAverageQuartets <- function (dataset, cex=1.1, pch=2, col='black', ...) {
   JoinTheDots(apply(dataset[, c('r2', 'd', 's'), ], 2, rowMeans),
               col=col, cex=cex, pch=pch, ...)
+}
+
+#' Convert splits data to ternary-plottable points
+#' @export
+SplitsToPoints <- function (itemData) {
+  rbind(itemData[, 'ref'] - itemData[, 'cf'],
+        itemData[, 'cf_not_ref'],
+        itemData[, 'cf_and_ref'])
+}
+
+#' @describeIn clPlotQuartets Splits equivalent of clPlotQuartets
+#' @export
+clPlotSplits <- function(dataset, tree, cex=1.1, pch=2, col=clColours, ...) {
+  lapply(names(dataset), function (weighting) {
+    JoinTheDots(SplitsToPoints(dataset[[weighting]][, , tree]),
+                col=col[weighting], cex=cex, pch=pch, ...)
+  })
+
+  # Return:
+  invisible()
+}
+
+#' @describeIn clPlotQuartets Splits equivalent of clPlotTheseAverageQuartets
+#' @export
+clPlotTheseAverageSplits <- function (dataset, cex=1.1, pch=2, col='black', ...) {
+  JoinTheDots(SplitsToPoints(apply(dataset[, , ], 2, rowMeans, na.rm=TRUE)),
+              col=col, cex=cex, pch=pch, ...)
+}
+
+#' @describeIn clPlotQuartets Splits equivalent of clPlotAverageQuartets
+#' @export
+clPlotAverageSplits <- function(dataset, cex=1.1, pch=2, col=clColours, ...) {
+  lapply(names(dataset), function (weighting) {
+    clPlotTheseAverageSplits(dataset[[weighting]], col=col[weighting],
+                               cex=cex, pch=pch, ...)
+  })
+
+  # Return:
+  invisible()
 }
