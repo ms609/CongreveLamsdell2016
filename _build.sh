4102501c4a4abf5b33b347b5906b88fc45b992d5
Martin Smith <martins@gmail.com>
2018-12-10 12:26:20 +0000

    Try installing in _build.sh

--- _build.sh
+++ _build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+Rscript -e "devtools::install()"
 Rscript -e "rmarkdown::render('vignettes/Compare-methods.Rmd', 'all', output_dir='./doc')"
 Rscript -e "rmarkdown::render('vignettes/Compare-node-supports.Rmd', 'all', output_dir='./doc')"
 Rscript -e "rmarkdown::render('vignettes/Conduct-analyses.Rmd', 'all', output_dir='./doc')"
